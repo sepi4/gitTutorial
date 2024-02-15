f0e22fd611741d356b446571b4c6aba23a74f2cc
sepi4 <polser1@gmail.com>
2024-02-15 23:16:52 +0200

    kissa

---
+++ main.js
@@ -0,0 +1,16 @@
+function kissaIstuu() {
+  console.log('Kissa istuu')
+}
+
+
+function kissaMaukuu() {
+  console.log('Miau')
+}
+
+
+function main() {
+  kissaIstuu()
+  kissaMaukuu()
+}
+
+main()
